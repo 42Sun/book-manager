1afff5bf1e7d52ae8611090a66b0db462ef479de
42Sun <i@sundingyi.com>
2021-07-15 01:46:33 +0800

    Initial commit

---
+++ src/test/java/com/sundingyi/libmanager/LibManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sundingyi.libmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibManagerApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
