68e3044cb67f82ff8f8611e7cf326650391e244a
42Sun <i@sundingyi.com>
2021-07-17 05:49:59 +0800

    feat chinese login alert

---
+++ src/main/java/com/sundingyi/libmanager/config/ReloadMessageConfig.java
@@ -0,0 +1,16 @@
+package com.sundingyi.libmanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class ReloadMessageConfig {
+    
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:org/springframework/security/messages_zh_CN");
+        return messageSource;
+    }
+}
